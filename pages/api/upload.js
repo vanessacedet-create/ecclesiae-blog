69be7acde67c4aee16a40f61ed57dc1ca322bf19
Editora Ecclesiae <vanessa.cedet@gmail.com>
2026-03-11 18:54:22 -0300

    corrige caminhos dos imports

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,5 +1,5 @@
 import { getServerSession } from 'next-auth/next'
-import { authOptions } from '../auth/[...nextauth]'
+import { authOptions } from './auth/[...nextauth]'
 
 export const config = {
   api: { bodyParser: { sizeLimit: '10mb' } }
