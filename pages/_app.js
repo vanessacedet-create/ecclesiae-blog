b07692e2121a149c2fdbb9e58270aca0f5ce8942
Editora Ecclesiae <vanessa.cedet@gmail.com>
2026-03-11 19:12:54 -0300

    corrige SessionProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
+import { SessionProvider } from 'next-auth/react'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
+```
+
+Salve com **Ctrl+S** e feche. Depois:
+```
+git add .
+git commit -m "corrige SessionProvider"
+git push
