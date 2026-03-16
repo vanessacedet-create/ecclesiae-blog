dbbf144f405d9c26f6da4b109c5bca18885719e5
vanessacedet-create <vanessa.cedet@gmail.com>
2026-03-16 20:10:58 -0300

    Update logo.js

--- pages/api/logo.js
+++ pages/api/logo.js
@@ -1,5 +1,5 @@
 import { getServerSession } from 'next-auth/next'
-import { authOptions } from '../auth/[...nextauth]'
+import { authOptions } from './auth/[...nextauth]'
 import { Octokit } from '@octokit/rest'
 
 const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN })
