19738e7f72eadf830dba6c5cbc9877a8f1459b68
vanessacedet-create <vanessa.cedet@gmail.com>
2026-03-23 14:19:29 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,22 @@
 import { SessionProvider } from 'next-auth/react'
+import Script from 'next/script'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
   return (
     <SessionProvider session={pageProps.session}>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-F162WWF7XQ"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-F162WWF7XQ');
+        `}
+      </Script>
       <Component {...pageProps} />
     </SessionProvider>
   )
